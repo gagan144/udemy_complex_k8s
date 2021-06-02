bec21663d997de5989b735810ae9fbb3ec14dd9a
Gagandeep <singh.gagan144@gmail.com>
2021-06-02 17:12:24 +0530

    Foundation Project Code

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+
+function App() {
+    return (
+        <Router>
+
+            <div className="App">
+                <header className="App-header" style={{"minHeight": "unset"}}>
+                    {/*<img src={logo} className="App-logo" alt="logo"/>*/}
+                    <h1>Udemy complex application</h1>
+                    <h2>Version: 1.0.6</h2>
+
+                    <div style={{"padding": "20px", "color": "white"}}>
+                        <Link to="/">Home</Link>
+                        &nbsp;&nbsp;
+                        <Link to="/otherpage">Other Page</Link>
+                    </div>
+                </header>
+
+                <div style={{"padding": "20px"}}>
+                    <Route exact path="/" component={Fib} />
+                    <Route path="/otherpage" component={OtherPage} />
+                </div>
+
+                <div align="center" style={{marginTop: '30px'}}>
+                    By: Gagandeep Singh
+                </div>
+            </div>
+
+        </Router>
+    );
+}
+
+export default App;
---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  // Commenting test case since App.js >  Fib.js requires backend server api
+
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
